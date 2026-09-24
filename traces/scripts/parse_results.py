e98b50f4c8987d8d7b051c0e16a5601aaaec6676
Ryan Ellis <ryan.ellis@uwaterloo.ca>
2026-09-23 20:38:12 -0400

    small fix

--- traces/scripts/parse_results.py
+++ traces/scripts/parse_results.py
@@ -23,7 +23,7 @@ def parse_log(filepath, csv_out=None, l1_out=None, debug=False):
     meta_re = re.compile(
         r"@ clk_cycle\s+(\d+): Req in MSHR; need dram\?: (\d+), need probe\? (\d+), evicting\? (\d+), "
         r"back-inv\? (\d+), source: (0x[0-9a-fA-F]+), set: (0x[0-9a-fA-F]+), tag: (0x[0-9a-fA-F]+), "
-        r"param: (\d+)",
+        r"(0x[0-9a-fA-F]+)",
         re.IGNORECASE)
     stall_re = re.compile(
         r"@ clk_cycle\s+(\d+): ReleaseData prevented from entering SinkC due to no putbuff space!",
@@ -284,7 +284,7 @@ def parse_log(filepath, csv_out=None, l1_out=None, debug=False):
                 src        = int(m[6], 16)
                 mshr_set   = int(m[7], 16)
                 tag        = int(m[8], 16)
-                param      = int(m[9])
+                param      = int(m[9], 16)
                 node_type  = "Store" if param in (1, 2) else "Load"   # NEW
 
                 # Look up the sink arrival time for this source so we can later
